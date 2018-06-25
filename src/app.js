a670d94f1071c8ee5b6e5a42203fa8757d0d5894
Daniel Childs <dc.1994@outlook.com>
2018-06-25 15:53:20 +0100

    make the app.js file

---
+++ src/app.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import WeeklyRealse from './containers/WeeklyRealse.js'
+
+class App extends Component{
+  render {
+    return (
+      <WeeklyRealse />
+    )
+  }
+}
+
+export default App
