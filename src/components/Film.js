925db3a3e3c41c094f8546f3cd6a35db5f347481
Daniel Childs <dc.1994@outlook.com>
2018-06-25 16:31:20 +0100

    can render data from film.js in weeklyrelease.js and add state and data objects

---
+++ src/components/Film.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+class Film extends Component {
+  render() {
+    return(
+      <div>
+        <ul>
+          <li>Hello World</li>
+        </ul>
+      </div>
+    )
+  }
+}
+
+export default Film;
--- src/containers/WeeklyRelease.js
+++ src/containers/WeeklyRelease.js
@@ -1,9 +1,28 @@
 import React, {Component} from 'react';
+import Film from '../components/Film.js'
 
 class WeeklyRelease extends Component{
+  constructor(props) {
+  super(props)
+  this.state = {
+    data: [
+      {id: 1, film: "Sausage Party"},
+      {id: 2, film: "Cafe Society"},
+      {id: 3, film: "Morgan"},
+      {id: 4, film: "The 9th Life of Louis Drax"},
+      {id: 5, film: "Naam Hai Akira"},
+      {id: 6, film: "Equity"},
+      {id: 7, film: "Things to come"}
+    ]
+  }
+}
+
   render() {
     return (
+      <div>
     <h1>Weekly Releases</h1>
+      <Film data={this.state.data}/>
+      </div>
     )
   }
 }
